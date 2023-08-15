54d1d86b2cf76b09fe44df5bd8c9462b14fd7bd9
Phonepaseuth <130568468+Black13311@users.noreply.github.com>
2023-08-15 13:14:22 +0700

    Create AdminController.php

---
+++ AdminController.php
@@ -0,0 +1,9 @@
+<?php
+class AdminController extends Controller
+{
+    public function dashboard()
+    {
+        // Add logic to display admin dashboard here
+        return view('admin.dashboard');
+    }
+}
