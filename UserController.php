772b16d24cfe684c1b2fa5079ff04f5dfe0a7a9a
Phonepaseuth <130568468+Black13311@users.noreply.github.com>
2023-08-15 13:13:42 +0700

    Create UserController.php
    UserController login

---
+++ UserController.php
@@ -0,0 +1,9 @@
+<?php
+class UserController extends Controller
+{
+    public function home()
+    {
+        // Add logic to display user home page here
+        return view('user.home');
+    }
+}
